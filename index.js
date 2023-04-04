e1c9e0d45263f98a934006f828df5dbdcb856a02
Wellington Menezes <ligton.ribeiro@gmail.com>
2023-04-04 11:41:42 -0300

    Example API - Express

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express(express.json());
+const PORT = 3000;
+
+app.get('/', (_, res) => {
+    res.status(200).json('Example API - IT Talent')
+});
+
+app.listen(PORT, () => {
+    console.log(`Example API listening on port ${PORT}`);
+});
