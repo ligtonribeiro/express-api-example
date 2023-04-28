19924adf0028e1cf46f46b0eb26f09f71b0ac392
Wellington Menezes <ligton.ribeiro@gmail.com>
2023-04-28 18:05:16 -0300

    corrigido teste route users

--- __tests__/routes/user.routes.test.ts
+++ __tests__/routes/user.routes.test.ts
@@ -4,6 +4,6 @@ import app from "../../src/app";
 describe("Users Routes", () => {
   it("Get all users", async () => {
     const res = await request(app).get("/users");
-    expect(res.body).toEqual(["Rosimeire", "Luanny", "Luan"]);
+    expect(res.body).toEqual(["Wellington", "Rosimeire", "Luanny", "Luan"]);
   });
 });
