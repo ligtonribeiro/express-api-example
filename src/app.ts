93a60eef3012f603a0d6c91e2dd31d3cc4bc6d8a
Wellington Menezes <ligton.ribeiro@gmail.com>
2023-04-28 13:06:50 -0300

    fix app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application, Request, Response, NextFunction } from "express";
 import { router as userRoutes } from "./routes/user.route";
 
 const app: Application = express();
+app.disable("x-power-by");
 
 app.use("/users", userRoutes);
 app.use("/", (req: Request, res: Response, next: NextFunction) => {
