2afb0ae40c571b4ce00d6cfb5d196a08214da7d3
Wellington Menezes <ligton.ribeiro@gmail.com>
2023-05-11 15:09:02 -0300

    fix tests

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -6,7 +6,14 @@ describe("Test app.ts", () => {
   it("Root route", async () => {
     const res = await request(app).get("/version");
     expect(res.body).toEqual({
-      version: "1.0.1-release",
+      version: "1.1.0-main",
+    });
+  });
+
+  it("Root route", async () => {
+    const res = await request(app).get("/");
+    expect(res.body).toEqual({
+      message: "Express and Jest Example - IT Talent Program",
     });
   });
 });
--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,11 @@ const app: Application = express();
 app.disable("x-power-by");
 
 app.use("/users", userRoutes);
-app.use("/", (req: Request, res: Response, next: NextFunction) => {
-  res.json({ message: "Express and Jest Example - IT Talent Program" });
-});
 app.use("/version", (req: Request, res: Response, next: NextFunction) => {
   res.json({ version: "1.1.0-main" });
+});
+app.use("/", (req: Request, res: Response, next: NextFunction) => {
+  res.json({ message: "Express and Jest Example - IT Talent Program" });
 });
 
 export default app;
