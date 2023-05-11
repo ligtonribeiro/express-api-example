3b6a1e99fe5ea80b2f9e1efb74ade452380483f7
Wellington Menezes <ligton.ribeiro@gmail.com>
2023-05-11 15:05:04 -0300

    update main

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,11 @@ const app: Application = express();
 app.disable("x-power-by");
 
 app.use("/users", userRoutes);
+app.use("/", (req: Request, res: Response, next: NextFunction) => {
+  res.json({ message: "Express and Jest Example - IT Talent Program" });
+});
 app.use("/version", (req: Request, res: Response, next: NextFunction) => {
-  res.json({ version: "1.0.1-release" });
+  res.json({ version: "1.1.0-main" });
 });
 
 export default app;
