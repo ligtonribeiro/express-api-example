e861178c126eb9b91015e7d596d0d6474962fa97
Wellington Menezes <ligton.ribeiro@gmail.com>
2023-06-30 16:10:01 -0300

    adicionado observalibilidade com Loki e Grafana

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,12 @@ import { router as userRoutes } from "./routes/user.route";
 const app: Application = express();
 app.disable("x-power-by");
 
+app.use((req, res, next) => {
+  console.log("Request Type:", req.method);
+  console.log("Time:", Date.now());
+  next();
+});
+
 app.use("/users", userRoutes);
 app.use("/version", (req: Request, res: Response, next: NextFunction) => {
   res.json({ version: "1.1.0-main" });
